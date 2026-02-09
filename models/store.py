07ddcce07eaf01b3b9eb2c130c4e02ffe5f208c6
Mjacm15 <alfamar2005@gmail.com>
2026-02-08 21:18:55 -0500

    Fix: spelling mistake fixed

--- models/store.py
+++ models/store.py
@@ -5,4 +5,4 @@ class Store(db.Model):
     __table_name__='store'
     store_id=db.Column(db.Integer, primary_key=True)
     manager_staff_id=db.Column(db.Integer, nullable=False)
-    adress_id=db.Column(db.Integer, nullable=False)
+    address_id=db.Column(db.Integer, nullable=False)
